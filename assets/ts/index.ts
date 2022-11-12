a1cfca7ca246bf8a31a2ecda676db61731bd2050
Akatsuki1910 <35941346+Akatsuki1910@users.noreply.github.com>
2022-11-13 01:18:11 +0900

    Update index.ts

--- assets/ts/index.ts
+++ assets/ts/index.ts
@@ -1,2 +1,6 @@
 export const r = (num: number, p: number) =>
   Math.round(num * Math.pow(10, p - 1)) / Math.pow(10, p - 1)
+
+export const dCount = (arr: number[], d: number[]) => {
+  return d.filter((v) => arr.includes(v)).length
+}
