639f467a48356f91fd94ea2ae515611c8d3f5619
Akatsuki1910 <35941346+Akatsuki1910@users.noreply.github.com>
2022-11-12 19:41:51 +0900

    Update Choice.vue

--- components/Choice.vue
+++ components/Choice.vue
@@ -1,5 +1,23 @@
 <script setup lang="ts">
 import { r } from '@/assets/ts/index'
+
+const scoreMap = new Map<number, number>()
+
+for (let i = 1; i <= Math.pow(6, 5); i++) {
+  let addScore = 0
+  for (let l = 0; l < 5; l++) {
+    const s = (Math.floor((i - 1) / Math.pow(6, l)) % 6) + 1
+    addScore += s
+  }
+  scoreMap.set(
+    addScore,
+    scoreMap.has(addScore) ? scoreMap.get(addScore)! + 1 : 1,
+  )
+}
+
+const scoreArr = [...scoreMap]
+  .sort((a, b) => (a[0] > b[0] ? 1 : -1))
+  .map((v) => [v[0], r(v[1] / Math.pow(6, 5), 5)])
 </script>
 
 <template lang="pug">
@@ -8,7 +26,7 @@ table
     tr
       th(colspan='2') choice
   tbody
-    tr(v-for='i in Math.pow(6, 5)', :key='`c_tr_${i}`')
-      td {{ (Math.floor((i - 1) / Math.pow(6, 4)) % 6) + 1 + '-' + ((Math.floor((i - 1) / Math.pow(6, 3)) % 6) + 1) + '-' + ((Math.floor((i - 1) / Math.pow(6, 2)) % 6) + 1) + '-' + ((Math.floor((i - 1) / Math.pow(6, 1)) % 6) + 1) + '-' + (((i - 1) % 6) + 1) }}
-      td {{ r(Math.pow(1 / 6, 5), 5) }}
+    tr(v-for='s in scoreArr', :key='`c_tr_${i}`')
+      td {{ s[0] }}
+      td {{ s[1] }}
 </template>
