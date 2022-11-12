0b8794c44c0fd81e6a6f6a3a1f677a8c19490d3a
Akatsuki1910 <35941346+Akatsuki1910@users.noreply.github.com>
2022-11-13 01:18:24 +0900

    Update app.vue

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@ import { r } from '@/assets/ts/index'
 
 const diceRef = ref<HTMLInputElement[]>([])
 const diceSRef = ref<HTMLOptionElement[]>([])
-const diceArr = ref<number[]>(Array(5).fill(0))
+const diceArr = ref<number[]>(Array(5).fill(1))
 const dice = () => {
   for (const i in diceArr.value) {
     if (!diceRef.value[i].checked) {
@@ -16,12 +16,22 @@ const select = () => {
     diceArr.value[i] = +diceSRef.value[i].value
   }
 }
+
+const sDiceRef = ref<number[]>([])
+const change = (i: number) => {
+  sDiceRef.value = []
+  for (const i in diceArr.value) {
+    if (diceRef.value[i].checked) {
+      sDiceRef.value.push(diceArr.value[i])
+    }
+  }
+}
 </script>
 
 <template lang="pug">
 .dices
   .dice(v-for='(d, i) in diceArr', :key='`dice_${i}`')
-    input(type='checkbox', :id='`d${i}`', ref='diceRef')
+    input(type='checkbox', :id='`d${i}`', ref='diceRef', @change='change(i)')
     label(:for='`d${i}`') {{ d }}
   button(@click='dice') dices
 .selects
@@ -34,7 +44,7 @@ div
   FullHouse
   FourNumbers
   SS
-  BS
+  BS(:dice='sDiceRef')
   Yotto
   Choice
 </template>
