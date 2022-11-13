dc750d44278626f93e0b056c6f72f424d6034b50
Akatsuki1910 <35941346+Akatsuki1910@users.noreply.github.com>
2022-11-13 14:40:04 +0900

    Update FullHouse.vue

--- components/FullHouse.vue
+++ components/FullHouse.vue
@@ -1,5 +1,40 @@
 <script setup lang="ts">
-import { r } from '@/assets/ts/index'
+import { dCount, r } from '@/assets/ts/index'
+
+interface Props {
+  dice: number[]
+}
+
+const props = withDefaults(defineProps<Props>(), { dice: () => [] })
+
+const diceArr = ref<[number, number[]][]>([])
+
+watch(
+  () => props.dice,
+  () => {
+    for (let i = 1; i <= 6; i++) {
+      const vArr: number[] = []
+      const c = props.dice.filter((v) => v === i).length
+      for (let l = 1; l <= 6; l++) {
+        const b = i === l ? 0 : props.dice.filter((v) => v === l).length
+
+        let v = r(
+          Math.pow(1 / 6, Math.max(3 - c, 0)) *
+            Math.pow(1 / 6, Math.max(2 - b, 0)),
+          5,
+        )
+
+        if (c > 3) v = 0
+        if (b > 2) v = 0
+        if (props.dice.length !== c + b) v = 0
+
+        vArr.push(v)
+      }
+      diceArr.value[i - 1] = [i, vArr]
+    }
+  },
+  { immediate: true },
+)
 </script>
 
 <template lang="pug">
@@ -9,9 +44,9 @@ table
       th(colspan='7') full house
   tbody
     tr
-      td 2個 / 3個
+      td 3個 / 2個
       td(v-for='i in 6', :key='`fh_td_${i}`') {{ i }}
-    tr(v-for='i in 6', :key='`fh_tr_${i}`')
-      td {{ i }}
-      td(v-for='l in 6', :key='`fh_td_${i}_${l}`') {{ r(Math.pow(1 / 6, 2) * Math.pow(1 / 6, 3), 5) }}
+    tr(v-for='(d, i) in diceArr', :key='`fh_tr_${i}`')
+      td {{ d[0] }}
+      td(v-for='(dd, l) in d[1]', :key='`fh_td_${i}_${l}`') {{ dd }}
 </template>
